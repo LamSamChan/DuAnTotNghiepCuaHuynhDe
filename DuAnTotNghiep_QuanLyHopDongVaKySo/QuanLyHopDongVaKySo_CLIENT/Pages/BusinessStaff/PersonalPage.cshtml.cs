6a3c18f5c30339d534e3bddd6f07735f5bbea6dd
CODINGTHAOGROGRAM <106610498+CODINGTHAOGROGRAM@users.noreply.github.com>
2023-10-16 02:53:59 +0700

    chỉnh sửa lại project

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Pages/BusinessStaff/PersonalPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyHopDongVaKySo_CLIENT.Pages.BusinessStaff
+{
+    public class PersonalPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
