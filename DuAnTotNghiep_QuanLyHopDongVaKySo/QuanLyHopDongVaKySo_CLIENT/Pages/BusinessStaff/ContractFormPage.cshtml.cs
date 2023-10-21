339d38155f2958f6c3c5cba0123e5258371c799a
CODINGTHAOGROGRAM <106610498+CODINGTHAOGROGRAM@users.noreply.github.com>
2023-10-21 14:50:04 +0700

    commit code

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Pages/BusinessStaff/ContractFormPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyHopDongVaKySo_CLIENT.Pages.BusinessStaff
+{
+    public class ContractFormPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
