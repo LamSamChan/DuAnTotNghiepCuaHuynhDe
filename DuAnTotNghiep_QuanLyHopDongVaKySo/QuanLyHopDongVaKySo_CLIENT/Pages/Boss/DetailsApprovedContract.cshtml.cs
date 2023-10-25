84b98676c027da465cb65db855da5aa76e15cd1b
CODINGTHAOGROGRAM <106610498+CODINGTHAOGROGRAM@users.noreply.github.com>
2023-10-26 04:23:58 +0700

    commit code 26/10
    reponsive cho các trang

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Pages/Boss/DetailsApprovedContract.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyHopDongVaKySo_CLIENT.Pages.Boss
+{
+    public class DetailsApprovedContractModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
