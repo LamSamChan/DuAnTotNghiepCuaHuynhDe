c43bbb607dbc7984a11d108023802b205a475a76
LamSamChan <chan94170@gmail.com>
2023-10-26 21:58:35 +0700

    sắp done forgot password, còn gửi mail mật khẩu mới

--- DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_API/Services/EmployeeService/IEmployeeSvc.cs
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_API/Services/EmployeeService/IEmployeeSvc.cs
@@ -8,12 +8,13 @@ namespace QuanLyHopDongVaKySo_API.Services.EmployeeService
     {
         Task<List<Employee>> GetAll();
         Task<Employee> GetById(string empID);
+        Task<Employee> GetByEmail(string empEmail);
         Task<string> AddNew(Employee employee);
         Task<string> Update(Employee employee);
         Task<string> IsFieldExist(Employee employee);
         Task<string> IsRoleOrPositonCheck(Employee employee);
         Task<Employee> Login(ViewLogin viewLogin);
         Task<int?> ChangePassword(string empId, ChangePassword changePassword);
-
+        Task<int> ForgotPassword(string newPassword, ForgotPassword forgotPassword);
     }
 }
---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_API/ViewModels/ForgotPassword.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyHopDongVaKySo_API.ViewModels
+{
+    public class ForgotPassword
+    {
+        [Required(ErrorMessage = "Hãy điền email !")]
+        [RegularExpression("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$",
+            ErrorMessage = "Email không hợp lệ !")]
+        public string Email { get; set; }
+    }
+}
