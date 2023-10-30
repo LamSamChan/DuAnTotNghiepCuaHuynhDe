fda8077c62f55f591420ce4967945bd506befb00
Deku <93598365+LamSamChan@users.noreply.github.com>
2023-10-31 03:04:17 +0700

    pdf to image

--- DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Program.cs
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Program.cs
@@ -1,18 +1,16 @@
-using QuanLyHopDongVaKySo_CLIENT.Services.ContractCoordinateService;
 using QuanLyHopDongVaKySo_CLIENT.Services.CustomerServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddScoped<IContractCoordinateService, ContractCoordinateService>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 //Configure api calling address
-builder.Services.AddHttpClient<ContractCoordinateService>(client =>
+/*builder.Services.AddHttpClient<ContractCoordinateService>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7286/swagger/index.html");
-});
+});*/
 builder.Services.AddHttpClient<CustomerService>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7286/swagger/index.html");
