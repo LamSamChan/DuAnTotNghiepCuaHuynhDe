f907265f982091998304770c0434b128899950d7
Trung_test <nrong157214@gmail.com>
2023-11-02 21:51:13 +0700

    thiếu 2 page mai gắn tiếp Phần Admin

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Pages/Auth/AddRoles_2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyHopDongVaKySo_CLIENT.Pages.Auth
+{
+    public class AddRoles_2ModelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
