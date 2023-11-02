9c664560bc5c93a5a339811fa3a1a51885237813
CODINGTHAOGROGRAM <106610498+CODINGTHAOGROGRAM@users.noreply.github.com>
2023-11-02 23:22:45 +0700

    success

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo.CLIENT/Views/Admin/AddPosition_2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyHopDongVaKySo_CLIENT.Pages.Auth
+{
+    public class AddPosition_2ModelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
