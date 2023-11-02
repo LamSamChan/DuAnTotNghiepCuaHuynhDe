98810602e8b1d52186fe48979a28cee6ef6f0c48
Trung_test <nrong157214@gmail.com>
2023-11-02 20:38:21 +0700

    Update

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Pages/Auth/AddRoles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyHopDongVaKySo_CLIENT.Pages.Auth
+{
+    public class AddRolesModelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
