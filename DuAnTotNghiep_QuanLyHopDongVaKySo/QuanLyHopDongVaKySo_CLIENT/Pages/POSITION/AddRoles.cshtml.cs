c2160e21b629e3e91c2260c1324d2ac4e7d89b66
Trung_test <nrong157214@gmail.com>
2023-11-02 16:56:56 +0700

    Add Position

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Pages/POSITION/AddRoles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyHopDongVaKySo_CLIENT.Pages.POSITION
+{
+    public class AddRolesModelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_CLIENT/Pages/POSITION/ListRoles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuanLyHopDongVaKySo_CLIENT.Pages.POSITION
+{
+    public class ListRolesModelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
