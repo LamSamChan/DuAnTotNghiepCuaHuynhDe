b7a3eef37530df69e08e6b45c9524db3f2814a52
CODINGTHAOGROGRAM <106610498+CODINGTHAOGROGRAM@users.noreply.github.com>
2023-11-02 22:06:23 +0700

    rebuild project fe

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo.CLIENT/Controllers/BusinessStaffController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyHopDongVaKySo.CLIENT.Controllers
+{
+    public class BusinessStaffController : Controller
+    {
+        private readonly ILogger<BusinessStaffController> _logger;
+
+        public BusinessStaffController(ILogger<BusinessStaffController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
