a776146b36b00b93db6359f456ceaab6c9bfc160
CODINGTHAOGROGRAM <106610498+CODINGTHAOGROGRAM@users.noreply.github.com>
2023-11-03 16:05:35 +0700

    Bổ sung cho boss và fix input checkbox

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo.CLIENT/wwwroot/js/AddCus.js
@@ -0,0 +1,27 @@
+﻿ 
+const enableInputCheckbox = document.getElementById('enable-input');
+const inpt1 = document.getElementById('inputNameBuss');
+const inpt2 = document.getElementById('inputPosition');
+const inpt3 = document.getElementById('certi');
+const inpt4 = document.getElementById('coreSDN');
+const inpt5 = document.getElementById('dateSDN');
+const inpt6 = document.getElementById('adSDN');
+//const dataInput = document.querySelectorAll('.form-control');
+const content = document.querySelector('.content');
+//enableInputCheckbox.addEventListener('change', function () {
+//    dataInput.disabled = !enableInputCheckbox.checked;
+//});
+enableInputCheckbox.addEventListener('change', function () {
+    inpt1.disabled = !enableInputCheckbox.checked;
+    inpt2.disabled = !enableInputCheckbox.checked;
+    inpt3.disabled = !enableInputCheckbox.checked;
+    inpt4.disabled = !enableInputCheckbox.checked;
+    inpt5.disabled = !enableInputCheckbox.checked;
+    inpt6.disabled = !enableInputCheckbox.checked;
+    //if (enableInputCheckbox.checked) {
+    //    content.classList.add('disabled');
+    //} else {
+    //    content.classList.remove('disabled');
+    //}  
+
+});
