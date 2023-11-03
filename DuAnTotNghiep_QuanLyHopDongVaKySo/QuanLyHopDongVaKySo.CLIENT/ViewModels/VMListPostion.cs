9e560809858c3f74317b0b899cbfe333758f882f
Deku <93598365+LamSamChan@users.noreply.github.com>
2023-11-03 13:59:30 +0700

    show postion

---
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo.CLIENT/ViewModels/VMListPostion.cs
@@ -0,0 +1,10 @@
+﻿using Model = QuanLyHopDongVaKySo_API.Models;
+
+namespace QuanLyHopDongVaKySo.CLIENT.ViewModels
+{
+    public class VMListPostion
+    {
+        public List<Model.Position>? Postions { get; set; }
+        public List<Model.Employee>? Employees { get; set; }
+    }
+}
