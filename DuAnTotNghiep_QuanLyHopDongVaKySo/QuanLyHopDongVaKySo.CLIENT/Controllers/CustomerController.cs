d4f268eb8e8e07a0bcf1a3dc5336ff74de003787
CODINGTHAOGROGRAM <106610498+CODINGTHAOGROGRAM@users.noreply.github.com>
2023-11-04 03:17:11 +0700

    commit code  tạo vùng kí cho customer

--- DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo.CLIENT/Controllers/CustomerController.cs
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo.CLIENT/Controllers/CustomerController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Imaging;
+using test.Models;
 
 namespace QuanLyHopDongVaKySo.CLIENT.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public CustomerController(IWebHostEnvironment hostingEnvironment, IHttpContextAccessor contextAccessor)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _contextAccessor = contextAccessor;
+        }
         public IActionResult Index()
         {
             return View();
@@ -11,6 +21,27 @@ namespace QuanLyHopDongVaKySo.CLIENT.Controllers
         public IActionResult CusToSign()
         {
             return View();
+        }
+        public IActionResult Log()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SaveSignature([FromBody] SignData sData)
+        {
+            if (null == sData)
+                return NotFound();
+
+            var bmpSign = SignUtility.GetSignatureBitmap(sData.Data, sData.Smooth, _contextAccessor, _hostingEnvironment);
+
+            var fileName = System.Guid.NewGuid() + ".png";
+            var filePath = Path.Combine(Path.Combine(_hostingEnvironment.WebRootPath, "Signatures"), fileName);
+
+            bmpSign.Save(filePath, ImageFormat.Png);
+
+            return Content(fileName);
         }
     }
 }
