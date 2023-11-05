82192c74d273b7c32734835a83d03844027b5c13
Deku <93598365+LamSamChan@users.noreply.github.com>
2023-11-06 01:28:54 +0700

    cả nhà ngủ ngon <3

--- DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo.CLIENT/Controllers/VerifyController.cs
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo.CLIENT/Controllers/VerifyController.cs
@@ -1,17 +1,22 @@
 ﻿// Ignore Spelling: auth
 
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using QuanLyHopDongVaKySo.CLIENT.Services;
+using QuanLyHopDongVaKySo.CLIENT.Services.RoleServices;
 using QuanLyHopDongVaKySo.CLIENT.ViewModels;
+using QuanLyHopDongVaKySo_CLIENT.Constants;
 
 namespace QuanLyHopDongVaKySo.CLIENT.Controllers
 {
     public class VerifyController : Controller
     {
         private readonly IAuthServices _authServices;
-        public VerifyController(IAuthServices authServices)
+        private readonly IRoleService _roleService;
+        public VerifyController(IAuthServices authServices, IRoleService roleService)
         {
             _authServices = authServices;
+            _roleService = roleService;
         }
         public IActionResult Index()
         {
@@ -20,9 +25,16 @@ namespace QuanLyHopDongVaKySo.CLIENT.Controllers
 
         public async Task<IActionResult> Login(VMLogin login)
         {
-            int reponse = await _authServices.Login(login);
-            if (reponse != 0)
+            var reponse = await _authServices.Login(login);
+            if (reponse != null)
             {
+                HttpContext.Session.SetString(SessionKey.Employee.EmployeeContext,
+                        JsonConvert.SerializeObject(reponse));
+                HttpContext.Session.SetString(SessionKey.Employee.EmployeeID,
+                        JsonConvert.SerializeObject(reponse.EmployeeId));
+                HttpContext.Session.SetString(SessionKey.Employee.Role,
+                        JsonConvert.SerializeObject(_roleService.GetRoleByIdAsync(reponse.RoleID).Result.RoleName));
+                
                 return RedirectToAction("Index", "BusinessStaff");
             }
             else
