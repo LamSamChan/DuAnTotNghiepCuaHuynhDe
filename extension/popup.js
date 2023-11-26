f417b1b8e141948498dcd2520f897fc0fda7dcf2
LamSamChan <chan94170@gmail.com>
2023-11-27 02:48:57 +0700

    upload extension

---
+++ extension/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("readToken").addEventListener("click", function () {
+    // Code để đọc USB token ở đây
+
+    console.log("ádsadsa");
+  });
+});
