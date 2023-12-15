5ebd8b56f19c5301e98182e324c2d2d01ffe3495
CODINGTHAOGROGRAM <106610498+CODINGTHAOGROGRAM@users.noreply.github.com>
2023-12-15 17:34:28 +0700

    push code chart item

--- DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_API/obj/Debug/net7.0/QuanLyHopDongVaKySo_API.AssemblyInfo.cs
+++ DuAnTotNghiep_QuanLyHopDongVaKySo/QuanLyHopDongVaKySo_API/obj/Debug/net7.0/QuanLyHopDongVaKySo_API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyHopDongVaKySo_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3561eba4c5013a9e2b19f85caf72f8ec5f342f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyHopDongVaKySo_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyHopDongVaKySo_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
